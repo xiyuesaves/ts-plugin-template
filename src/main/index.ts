d5baee39a7ea4faa445595eac76127556bc3b796
xiyuesaves <xiyuesaves@gmail.com>
2026-02-22 00:33:20 +0800

    chore: 添加注释

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,11 @@
 import type { BrowserWindow } from "electron";
 
+// 在窗口创建时触发
 function onBrowserWindowCreated(window: BrowserWindow) {
   console.log("onBrowserWindowCreated");
 }
 
+// 环境判断
 if ("qwqnt" in globalThis) {
   qwqnt.main.hooks.whenBrowserWindowCreated.peek(onBrowserWindowCreated);
 }
--- src/renderer/pages/settings.ts
+++ src/renderer/pages/settings.ts
@@ -1,3 +1,4 @@
+// 初始化设置界面，仅在setting窗口执行
 async function initSettingView(view: HTMLDivElement) {
   const devInfo: HTMLDivElement = document.createElement("div");
   devInfo.innerHTML = `Hello World!`;
